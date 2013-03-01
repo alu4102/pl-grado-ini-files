d617d47f24118a80cb4e4c5c9798e2aa7eff9493
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-03-01 08:10:34 +0000

    changed parameter names in the template

--- ini.js
+++ ini.js
@@ -26,14 +26,14 @@ function calculate(evt) {
   }
 }
 
-var temp = '<li> <span class = "<%= t.type %>"> <%= s %> </span>\n';
+var temp = '<li> <span class = "<%= token.type %>"> <%= match %> </span>\n';
 
 function tokensToString(tokens) {
    var r = '';
    for(var i in tokens) {
      var t = tokens[i];
      var s = JSON.stringify(t, undefined, 2);
-     s = _.template(temp, {t: t, s: s});
+     s = _.template(temp, {token: t, match: s});
      r += s;
    }
    return '<ol>\n'+r+'</ol>';
